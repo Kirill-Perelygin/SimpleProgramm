764a626b4478db00194de4110ea7fc82ab9d3cf2
Kirill Perelygin <perelygin.kirill@gmail.com>
2024-12-07 21:49:06 +0300

    My first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        while (true) {
+            System.out.println("Введите первое число");
+            Scanner scanner = new Scanner(System.in);
+            a = scanner.nextInt();
+            System.out.println("Введите второе число");
+            b = scanner.nextInt();
+            c = a + b;
+            System.out.println("Сумма введенных чисел равняется " + c);
+        }
+    }
+}
